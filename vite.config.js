8c113072efb40672960b22ea3ab5ba415d362d3b
Tainá Kasai <tainakasai01@gmail.com>
2025-10-26 15:52:27 -0300

    build: Adiciona comando de deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/front-end",
   server: {
     host: true,        // aceita conexões externas (0.0.0.0)
     port: 3001,
@@ -11,4 +12,5 @@ export default defineConfig({
       clientPort: 443  // força o cliente a conectar na porta 443 (útil para proxies/túneis)
     }
   }
+  
 })
